3b93132c608daa98672f70ba1cf0851b48e085bb
“KEEVIN <keevin.mitchell@gmail.com>
2015-01-30 18:40:42 -0500

    Initial Commit

---
+++ KVODemoObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVODemoObjectiveC
+//
+//  Created by KEEVIN MITCHELL on 1/30/15.
+//  Copyright (c) 2015 Beyond 2021. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVODemoObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVODemoObjectiveC
+//
+//  Created by KEEVIN MITCHELL on 1/30/15.
+//  Copyright (c) 2015 Beyond 2021. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
